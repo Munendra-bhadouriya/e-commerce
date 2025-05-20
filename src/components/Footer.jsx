69ae6f7ceed767ff5869f63f775870d0302e0be9
Munendra-bhadouriya <munendra.connect@gmail.com>
2025-05-21 00:36:53 +0530

    made final changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
+              <a  href="https://github.com/Munendra-bhadouriya" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Munendra</a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
